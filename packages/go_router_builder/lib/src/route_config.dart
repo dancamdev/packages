0e0b86ef756dcd4516a3814994b172ebd404be8b
Daniele Cambi <dancam.dev@gmail.com>
2023-04-13 17:49:07 +0200

    removed name from ShellRoute

--- packages/go_router_builder/lib/src/route_config.dart
+++ packages/go_router_builder/lib/src/route_config.dart
@@ -338,7 +338,6 @@ routes: [${_children.map((RouteConfig e) => '${e._routeDefinition()},').join()}]
     if (_isShellRoute) {
       return '''
   ShellRouteData.\$route(
-    ${_name != null ? 'name: ${escapeDartString(_name!)},' : ''}
     factory: $_extensionName._fromState,
     $navigatorKey
     $routesBit
